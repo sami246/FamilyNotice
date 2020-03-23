894a5e15bfcaf6ed4f489e98330b9bbc41ce4f43
sami246 <46711771+sami246@users.noreply.github.com>
2020-03-23 21:58:53 +0000

    Added base dir to settings

--- FamilyNotice/settings.py
+++ FamilyNotice/settings.py
@@ -53,13 +53,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'FamilyNotice.urls'
@@ -138,6 +139,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
 LOGIN_REDIRECT_URL = "choose family" #redirect view because view is built in
 LOGOUT_REDIRECT_URL = "login" # redirect when user logsout
